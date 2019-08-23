0ae1cdfa5e5cb21bad73a522e1e08ab2538b2651
sist <sist@DESKTOP-7UE7LE3>
2019-08-23 17:11:36 +0900

    재수정

--- Test/src/main/java/com/test/App.java
+++ Test/src/main/java/com/test/App.java
@@ -10,6 +10,6 @@ public class App
     {
     	
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); 
     }
 }
