6eac010e65c8c58112384665112e0a7a3af5d4a9
sist <sist@DESKTOP-7UE7LE3>
2019-08-23 17:13:58 +0900

    수우우우정

--- Test/src/main/java/com/test/App.java
+++ Test/src/main/java/com/test/App.java
@@ -12,6 +12,5 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" ); 
         System.out.println( "Hello World!" ); 
-        System.out.println( "Hello World!" ); 
     }
 }
