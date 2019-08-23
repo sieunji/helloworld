659955aa735b57db5d160e369c055b6960f19195
sieunji <54430726+sieunji@users.noreply.github.com>
2019-08-23 16:47:35 +0900

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.ehr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
